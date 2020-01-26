f7ea54b656b408f25d55a604a0bde0b4a9a5d712
Jeffrey Brown <brownjs03@gmail.com>
2020-01-26 19:58:29 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end 
   
   def self.most_popular_show 
-    self.where("rating = highest_rating").first
+    self.where("rating = ?", highest_rating").first
   end 
   
 end 
